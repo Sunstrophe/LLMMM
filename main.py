e89756690bd86e1a01f5a0614c7282de2f3a1ede
Sunstrophe <mackansund_2@live.se>
2024-10-21 20:05:59 +0200

    Rewriting code

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-import database.milvusdb as mdb
+import logging
+from pymilvus import MilvusClient
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+DB_NAME = "airene_memory"
+
+
+client = MilvusClient(DB_NAME)
+
+if not client.has_collection("people"):
+    client.create_collection(
+        collection_name="people",
+        dimension=384
+    )
+    logging.info("Created collection 'people'")
+
+
+def search_memory(collection, search_string: str) -> list:
+    output = client.search(
+        collection_name=collection,
+
+    )
+    return output
+
+
+def add_memory(memories: list):
+    ...
