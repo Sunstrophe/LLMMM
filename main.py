8882212977bd4658d673f788cb61b1bd9c0abb29
Sunstrophe <mackansund_2@live.se>
2024-10-21 22:52:29 +0200

    Implemented ability to call ai model

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import logging
 from pymilvus import MilvusClient
+from langchain_openai import ChatOpenAI, OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
+os.environ.get()
+
 
 DB_NAME = "airene_memory"
+OPENAI_KEY = os.getenv("OPENAI_API")
 
+llm = OpenAI()
+chat_model = ChatOpenAI(api_key=OPENAI_KEY, model="gpt-3.5-turbo-0125")
 
 client = MilvusClient(DB_NAME)
 
@@ -29,3 +39,4 @@ def search_memory(collection, search_string: str) -> list:
 
 def add_memory(memories: list):
     ...
+
